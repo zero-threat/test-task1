1d9075ba03213309ef77501cf6efeaa518e72f3d
Gennady Voznyuk <lilg.da.chip@gmail.com>
2020-01-08 21:26:05 +0300

    qwert

--- src/playerBuilder.js
+++ src/playerBuilder.js
@@ -8,16 +8,14 @@ const elementCreater = (tagName, className, src) => {
 };
 class SoundPlayer {
   constructor(rootElement = document.body) {
-    this.speedList = [
-      '1x',
-      '1.5x',
-      '2x'
-    ]
+    this.speedList = {
+      '1x': 1,
+      '1.5x': 1.5,
+      '2x': 2
+    }
     this.rootElement = rootElement;
     this.mediaElement = document.createElement('audio');
     this.mediaElement.volume = 1
-    this.currentProgressPosition = '-100'
-    this.speed = 1;
     this.renderPlayer();
   }
 
@@ -37,25 +35,10 @@ class SoundPlayer {
     const wrapperChildrens = [this.title, toolBar];
     wrapperChildrens.forEach(el => wrapper.appendChild(el))
 
-    this.playButton = elementCreater('div', `${this.classPrefix}-play-button`)
-    this.playButton.addEventListener('click', () => {
-      if (this.uploadInput.value != '') {
-        this.playAudio()
-      }
-    })
-    
-    this.stopButton = elementCreater('div', `${this.classPrefix}-stop-button`)
-    const stopButtonImage = elementCreater('img', null, 'https://cdn2.iconfinder.com/data/icons/font-awesome/1792/pause-circle-512.png')
-    this.stopButton.appendChild(stopButtonImage)
-    this.stopButton.addEventListener('click', () => this.playAudio())
+    this.createPlayButton()
 
     this.currentTimeSpan = elementCreater('span', `${this.classPrefix}-current-time`)
     this.currentTimeSpan.innerHTML = '0:00'
-    const progressWrapper = elementCreater('div', `${this.classPrefix}-progress-wrapper`);
-    const progressLine = elementCreater('div', `${this.classPrefix}-progress-line`);
-    this.progressBar = elementCreater('div', `${this.classPrefix}-progress-bar`)
-    this.progressBar.style.transform = `translate(calc(${this.currentProgressPosition}% + 5px), -50%)`
-    this.progressBarValue = this.currentProgressPosition
 
     this.timeLeftSpan = elementCreater('span', `${this.classPrefix}-time-left`)
     this.timeLeftSpan.innerHTML = '0:00'
@@ -63,7 +46,7 @@ class SoundPlayer {
       this.timeLeftSpan.innerHTML = this.getAudioDuration(this.mediaElement.duration)
     })
 
-    this.speedViewer = this.createSpeedChanger()
+    const speedViewer = this.createSpeedChanger()
     const volumeViewer = this.createVolumeChanger();
     
     this.downloadButton = elementCreater('button', `${this.classPrefix}-download-button`)
@@ -83,102 +66,150 @@ class SoundPlayer {
     uploadWrapper.appendChild(uploadLabel)
     const uploadImage = elementCreater('img', null, 'https://i-love-png.com/images/downloadicon.png');
     uploadLabel.appendChild(uploadImage)
+    const timeProgressBar = this.createProgressBar((position, e) => {
+      this.mediaElement.currentTime = (position / (e.target.clientWidth / 100)) * (this.mediaElement.duration / 100)
+      if (this.mediaElement.paused) {
+        this.playAudio()
+      }
+    })
 
     const toolBarChildrens = [
       this.playButton,
       this.stopButton,
       this.currentTimeSpan,
-      progressWrapper,
+      timeProgressBar,
       this.timeLeftSpan,
-      this.speedViewer,
+      speedViewer,
       volumeViewer,
       this.downloadButton,
       uploadWrapper
     ];
     toolBarChildrens.forEach(el => toolBar.appendChild(el))
   
+  }
+
+  createProgressBar = (onClick) => {
+    const progressWrapper = elementCreater('div', `${this.classPrefix}-progress-wrapper`);
+    const progressLine = elementCreater('div', `${this.classPrefix}-progress-line`)
+    progressLine.addEventListener('click', (e) => {
+      const lineRect = e.target.getBoundingClientRect() 
+      const position = (e.clientX - lineRect.x)
+      this.progressBarUpdate(position)
+      onClick(position, e)
+    })
+    this.progressBar = elementCreater('div', `${this.classPrefix}-progress-bar`)
     const progressWrapperChildrens = [
       this.progressBar,
       progressLine
     ];
     progressWrapperChildrens.forEach(el => progressWrapper.appendChild(el))
-
+    return progressWrapper
   }
 
   createSpeedChanger = () => {
     const speedViewer = elementCreater('button', `${this.classPrefix}-speed-changer`);
-    speedViewer.innerHTML = this.speedList[0]
+    const currentSpeed = elementCreater('span')
+    currentSpeed.innerHTML = '1x'
+    speedViewer.appendChild(currentSpeed)
+    const speedChangerMenu = elementCreater('div', `${this.classPrefix}-speed-changer-menu`)
+    speedViewer.appendChild(speedChangerMenu)
+    Object.keys(this.speedList).reverse().forEach((key) => {
+      const speedButton = elementCreater('button')
+      speedButton.innerHTML = key;
+      speedButton.addEventListener('click', (e) => {
+        this.mediaElement.playbackRate = this.speedList[key]
+        currentSpeed.innerHTML = key
+      })
+      speedChangerMenu.appendChild(speedButton)
+    })
     return speedViewer;
   }
 
   createVolumeChanger = () => {
     const volumeViewer = elementCreater('div', `${this.classPrefix}-volume-changer`);
-    this.image = elementCreater('img');
-    volumeViewer.addEventListener('click', () => {
-      this.image.src = 'https://png.pngtree.com/svg/20160628/abd7f14a9d.svg'
+    const volumeProgressBar = this.createProgressBar(() => {
+      
+    })
+    volumeViewer.appendChild(volumeProgressBar)
+    
+    const image = elementCreater('img');
+    image.addEventListener('click', () => {
+      image.src = 'https://png.pngtree.com/svg/20160628/abd7f14a9d.svg'
       if (this.mediaElement.volume == 1) {
         this.mediaElement.volume = 0
       } else {
         this.mediaElement.volume = 1
-        this.image.src = 'https://cdn2.iconfinder.com/data/icons/flat-ui-icons-24-px/24/volume-24-512.png'
+        image.src = 'https://cdn2.iconfinder.com/data/icons/flat-ui-icons-24-px/24/volume-24-512.png'
       }
     })
-    this.image.src = 'https://cdn2.iconfinder.com/data/icons/flat-ui-icons-24-px/24/volume-24-512.png'
-    volumeViewer.appendChild(this.image)
+    image.src = 'https://cdn2.iconfinder.com/data/icons/flat-ui-icons-24-px/24/volume-24-512.png'
+    volumeViewer.appendChild(image)
     return volumeViewer;
   }
 
   
 
   playAudio = () => {
-    if (this.isPlaying == false) {
-      this.mediaElement.play()
-      this.isPlaying = true
-      this.playButton.style.display = 'none'
-      this.stopButton.style.display = 'block'
-      this.timerId = setInterval(() => {
-        this.currentTimeSpan.innerHTML = this.getAudioDuration(this.mediaElement.currentTime)
-        this.timeLeftSpan.innerHTML = this.getAudioDuration((this.mediaElement.duration - this.mediaElement.currentTime))
-        this.progressBar.style.transform = this.progressBarValue
-        this.progressBarUpdate(this.mediaElement.duration, this.currentProgressPosition)
-        console.log(this.progressBar.style.transform)
-      }, 1000)
-    } else {
-      this.mediaElement.pause();
-      this.isPlaying = false
-      this.playButton.style.display = 'block'
-      this.stopButton.style.display = 'none'
-      clearInterval(this.timerId)
-    }
+    this.playButton.style.display = 'none'
+    this.stopButton.style.display = 'block'
+    this.mediaElement.addEventListener('timeupdate', this.playerUpdate)
+    this.mediaElement.play()
+  }
+
+  stopAudio = () => {
+    this.mediaElement.pause();
+    this.playButton.style.display = 'block'
+    this.stopButton.style.display = 'none'
+  }
+
+  createPlayButton = () => {
+    this.playButton = elementCreater('div', `${this.classPrefix}-play-button`)
+    this.playButton.addEventListener('click', () => {
+      if (this.uploadInput.value != '') {
+        this.playAudio()
+      }
+    })
+
+    this.stopButton = elementCreater('div', `${this.classPrefix}-stop-button`)
+    const stopButtonImage = elementCreater('img', null, 'https://cdn2.iconfinder.com/data/icons/font-awesome/1792/pause-circle-512.png')
+    this.stopButton.appendChild(stopButtonImage)
+    this.stopButton.addEventListener('click', () => this.stopAudio())
   }
 
   onAudioEnded = () => {
     this.currentTimeSpan.innerHTML = '0:00'
     this.timeLeftSpan.innerHTML = '0:00'
-    this.isPlaying = false
     this.playButton.style.display = 'block'
     this.stopButton.style.display = 'none'
-    clearInterval(this.timerId)
+    this.progressBarUpdate(0)
+
+    this.mediaElement.removeEventListener('timeupdate', this.playerUpdate)
   }
 
-  // progressBarUpdate = () => {
-  //   this.currentTime
-  //   this.progressBar
-  // }
+  playerUpdate = () => {
+    const trackTimePercents = (this.mediaElement.currentTime / (this.mediaElement.duration / 100)) 
+    const progressBarPosition = (
+      trackTimePercents * (this.progressBar.clientWidth / 100)
+    )
+    this.progressBarUpdate(progressBarPosition)
+    this.updateTimeSpans()
+  }
 
   getAudioDuration = (duration) => {
-    const correctDuration = [duration].toLocaleString().replace(/\,/g, "") -2
-    const minutes = Math.floor(correctDuration / 60000);
-    const seconds = ((correctDuration % 60000) / 1000).toFixed(0);
-    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+    const minutes = Math.floor(duration / 60);
+    const seconds = Math.floor(duration) - (minutes * 60)
+    return `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
   }
 
-  progressBarUpdate = (duration, position) => {
-    const progressBarTime = duration / 10
-    const previousPosition = +position + progressBarTime
-    this.progressBarValue = `translate(calc(${+position + previousPosition}% + 5px), -50%)`
-    // this.newPosition = this.positionUpdate 
-    // console.log(this.progressBarTime)
+  updateTimeSpans = () => {
+    this.timeLeftSpan.innerHTML = this.getAudioDuration(this.mediaElement.duration)
+    this.currentTimeSpan.innerHTML = this.getAudioDuration(this.mediaElement.currentTime)
+    this.timeLeftSpan.innerHTML = this.getAudioDuration((this.mediaElement.duration - this.mediaElement.currentTime))
+  }
+
+  progressBarUpdate = (position) => {
+    this.progressBar.style.left = position + 'px'
+    console.log(this.progressBar.style.left)
   }
   
   onSpeedChange = (speed) => {
